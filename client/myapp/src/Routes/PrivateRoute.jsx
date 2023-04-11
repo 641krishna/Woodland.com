6ec20f1183378ad930787c7ab875b6989604f2fc
Shrkrishna Aluri <shrikrishnaaluri641@gmail.com>
2023-04-11 22:03:47 +0530

    make it better

--- client/myapp/src/Routes/PrivateRoute.jsx
+++ client/myapp/src/Routes/PrivateRoute.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from 'react';
+import { Navigate, useLocation } from 'react-router-dom';
+import { shallowEqual, useSelector } from "react-redux";
 
-const PrivateRoute = () => {
-  return (
-    <div>PrivateRoute</div>
-  )
+const PrivateRoute = ({ children }) => {
+    const location = useLocation();
+
+    const { data } = useSelector((store) => {
+        return {
+            isLoading: store.auth.userLogin.isLoading,
+            isError: store.auth.userLogin.isError,
+            message: store.auth.userLogin.message,
+            data: store.auth.data,
+        };
+    }, shallowEqual);
+
+    if (!data.isAuthenticated) {
+        return <Navigate to='/login' state={{ from: location.pathname }} replace />
+    }
+    return children;
 }
 
-export default PrivateRoute
+export default PrivateRoute;
