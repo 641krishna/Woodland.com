dc62892f1880071accc204dc584dd2eac815e7dd
Shrkrishna Aluri <shrikrishnaaluri641@gmail.com>
2023-04-15 23:42:07 +0530

    make it better

--- client/myapp/src/Pages/ProductsPage.jsx
+++ client/myapp/src/Pages/ProductsPage.jsx
@@ -1,4 +1,35 @@
-import React from 'react'
+import React from 'react';
+import { ChevronRightIcon } from "@chakra-ui/icons";
+import {
+  Box,
+  Button,
+  ButtonGroup,
+  Card,
+  CardBody,
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  CardFooter,
+  Divider,
+  Image,
+  SimpleGrid,
+  Stack,
+  Text,
+  useToast,
+  Skeleton,
+  Heading,
+  Flex,
+} from "@chakra-ui/react";
+import React, { useEffect, useState } from "react";
+import { RiShoppingCart2Line } from "react-icons/ri";
+import { TbShoppingCartOff } from "react-icons/tb";
+import { shallowEqual, useDispatch, useSelector } from "react-redux";
+import {
+  Link,
+  useLocation,
+  useNavigate,
+  useSearchParams,
+} from "react-router-dom";
 
 const ProductsPage = () => {
   return (
